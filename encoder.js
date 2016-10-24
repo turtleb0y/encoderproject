f4a9815c2a847112c027da9b4bc57bb0d079ec16
Miguel Angel Castillo <321angelcastillo.mc@gmail.com>
2016-10-24 11:36:05 -0700

    Got some help. . .

--- encoder.js
+++ encoder.js
@@ -1,4 +1,9 @@
-$("#inputArea").keydown(function(e)) {
+$("#inputText").keyup(function(e) {
   var inputText = document.getElementById("inputText").value;
   document.getElementById("textArea").innerHTML = inputText;
+});
+
+$("input[type='radio']").click(function(e) {
+  $("input:checked").prop('checked', false );
+  $(this).prop('checked', true );
 });
